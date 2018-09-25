5a3faaccfec295b9f2c67ea34b9b7d2e3c1679d8
Zorry <jesusemanuelzorrilla@gmail.com>
2018-09-24 22:02:26 -0300

    casi una depuracion total
    faltan detalles

--- js/player.js
+++ js/player.js
@@ -1,4 +1,4 @@
-function Jugador(CantF, NumJ, Name,offsetY,offsetX,color){
+function Jugador(NumJ, Name,offsetY,offsetX,color){
   this.fichas = new Array(21);
   this.turno = false;
   this.numero = NumJ;
@@ -31,18 +31,17 @@ Jugador.prototype.moverFicha = function(x,y,ctx,ficha) {
 Jugador.prototype.misFichas = function (x,y) { // devuelve true en caso de que el click sea en una de sus fichas
   for (var i = 0; i < this.fichas.length; i++) {
     if (this.fichas[i].seleccion(x,y,this.fichas[i])){
-        console.log(i);
-        return i;
+          return i;
     }
   }
   return -1;
 };
 Jugador.prototype.jugar = function (){
     if (this.fichas.length > 0){
-    index = this.fichas.length;
-    this.fichas.splice(this.fichas[index], 1);
-    this.dibFichaP();
-    return true;
+      index = this.fichas.length;
+      this.fichas.splice(this.fichas[index], 1);
+      this.dibFichaP();
+          return true;
 
   }
   return false;
